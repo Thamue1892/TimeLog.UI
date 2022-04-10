47483b9cf723591513b10105ca1a47df9a58094b
Muzi <muzingayed@phumelela.com>
2022-04-10 14:34:54 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TimeLog.UI',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
